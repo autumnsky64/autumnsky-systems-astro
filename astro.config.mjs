0ee397e65c96ac482434c4003ce280e6d9448d43
akiya <autumnsky64@gmail.com>
2023-10-28 11:47:41 +0900

    Tailwind を導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [ tailwind({
+  })],
 });
