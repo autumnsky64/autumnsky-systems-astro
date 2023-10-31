b3e15b444282f37eacf90ae7e7da44f1950e619a
akiya <autumnsky64@gmail.com>
2023-10-31 09:57:19 +0900

    monofur フォントを組み込み

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import plugin from 'tailwindcss'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -6,9 +8,22 @@ module.exports = {
 		colors: {
 			'nakano-hanada': '#436fb4',
 			'hi-hanada': '#257bcc',
+		},
+		fontFamily: {
+			"monofur": ['monofur', 'ui-sans-serif']
 		}
 	},
   },
-  plugins: [],
+  plugins: [
+    plugin( ({ addBase }) => {
+      addBase({
+        '@font-face': {
+            fontFamily: 'monofur',
+            fontWeight: '300',
+            src: 'url(./src/fonts/monof.woff2) format("woff2"), url(./src/fonts/monof.woff) format("woff")'
+        }
+      })
+    }),
+  ],
 };
 
