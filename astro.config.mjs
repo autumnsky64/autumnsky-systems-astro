de3931c8091704fc28109565a27a86c1dc2c8b59
akiya <autumnsky64@gmail.com>
2023-11-17 10:51:57 +0900

    スタティックなサイト出力のための調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  outDir: "./dist",
   integrations: [ tailwind({
   })],
 });
